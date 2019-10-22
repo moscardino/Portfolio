e5de2edf80b9a98d26e73c6a04cb9eee32e643e6
VITTORI Francesco <fvittori@murex.com>
2019-10-22 16:12:49 +0200

    sdg

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -1,10 +1,26 @@
 import React, { Fragment } from 'react'
+import ReactTooltip from 'react-tooltip'
 
-export default ({ title = 'about', text = '' }) => {
+export default ({ title = 'about', whoIam }) => {
   return (
     <Fragment>
       <h1>{title}</h1>
-      <p dangerouslySetInnerHTML={{ __html: text }}></p>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {whoIam.map(character => (
+          <div style={{ width: '6rem' }}>
+            <ReactTooltip id={character.whatIam} type="light">
+              <div style={{width: '6rem'}}>{character.details}</div>
+            </ReactTooltip>
+            <img
+              src={character.image}
+              alt={character.details}
+              data-tip=""
+              data-for={character.whatIam}
+            />
+            <strong style={{fontSize: '0.8rem'}}>{character.whatIam}</strong>
+          </div>
+        ))}
+      </div>
     </Fragment>
   )
 }
