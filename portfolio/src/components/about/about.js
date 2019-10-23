77eea9c486319e7e9a72866a77ec68bd69b8f7bd
VITTORI Francesco <fvittori@murex.com>
2019-10-23 11:09:07 +0200

    qsdgsdg

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -2,46 +2,85 @@ import React, { Fragment } from 'react'
 import ReactTooltip from 'react-tooltip'
 import styled from 'styled-components'
 
-const Styledimg = styled.img`
+const Container = styled.div`
+  position: relative;
   width: 8rem;
-  height: 100%;
-  background-color: white;
-  transition: transform 0.2s;
+  height: 8rem;
+  border-radius:10px;
+  transition: transform 0.2s, opacity 0.2s;
+  text-align: center;
   :hover {
-    transform: scale(3);
-    background-color: rgb(236, 234, 234);
+    transform: scale(4);
+    z-index: 2;
+    background-color: white;
   }
-  :hover:after {
-    content: 'pipsdhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhpo';
+  :hover .image {
+    opacity: 0.2;
+    background-color: lightgrey;
+  }
+  .image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    border-radius:10px;
+    opacity: 1;
+    background-color: transparent;
+    transition:  opacity 0.2s ease-out;
+  }
+  .text {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    height: 100%;
+    width: 100%;
+    padding:5px;
+    border-radius:10px;
+    transition: opacity 0.2s ease-in;
+    font-size: 0.4rem;
+    overflow-bottom:hidden;
+    line-height: 1.1;
+    text-align: center;
+  }
+  .text:hover {
+    opacity: 1;
+  }
+  .separator{
+    border-style:solid;
+    border-width: 1px;
+    margin-bottom: 10px;
+    margin-left:auto;
+    margin-right:auto;
+    width:70%;
   }
 `
-const Styledstrong= styled.strong`
-font-size: 2rem;
-width: 24rem`
 
-const Styleddiv= styled.div`
-font-size: 1.5rem;
-width: 24rem`
+const Styledstrong = styled.strong`
+  font-size: 0.6rem;
+`
 
-const About = ({ title = 'Who Am I?', whoIam }) => {
+const About = ({ className, title = 'Who Am I?', whoIam }) => {
   return (
-    <Fragment>
+    <Fragment className={className}>
       <h1>{title}</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {whoIam.map(character => (
-          <div>
-            <ReactTooltip id={character.whatIam} type="light" multiline>
-              <Styledstrong>{character.whatIam}</Styledstrong>
-              <Styleddiv>{character.details}</Styleddiv>
-            </ReactTooltip>
-            <Styledimg
+          <Container>
+            <img
+              className="image"
               src={character.image}
               alt={character.details}
-              data-tip=""
-              data-for={character.whatIam}
-              className="image__about"
             />
-          </div>
+            <div className="text">
+              <Styledstrong>{character.whatIam}</Styledstrong>
+              <hr className='separator'/>
+              {character.details}
+            </div>
+          </Container>
         ))}
       </div>
     </Fragment>
